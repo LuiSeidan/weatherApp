f012a0e4ea1bca5fac2434baa4b97fb79f5087f5
Luis Ignacio Seidan <42916650+LuisSeidan@users.noreply.github.com>
2018-10-15 14:06:51 -0300

    Add files via upload

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
-import { SNOW } from '../constants/Weathers';
+import PropTypes from 'prop-types';
 import './styles.css';
 
-const WeatherData = () => (
+const WeatherData = ({data}) => {
+    const{temperature, weatherState, humidity, wind} = data;
+    return(
     <div className='weatherDataCont'>
-        <WeatherTemperature temperature={20} weatherState={SNOW} />
-        <WeatherExtraInfo humidity={80} wind={'10/ms'} />
-    </div>
-);
+        <WeatherTemperature temperature={temperature} weatherState={weatherState} />
+        <WeatherExtraInfo humidity={humidity} wind={wind} />
+    </div>)
+};
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+};
 
 export default WeatherData;
--- src/components/WeatherLocation.js
+++ src/components/WeatherLocation.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import { SUN, CLOUDY } from '../constants/Weathers';
 
-const WeatherLocation = () => (
-    <div className='weatherLocationCont'>
-        <Location city={'Buenos Aires!'}/>
-        <WeatherData />
-    </div>
-)
+const data1 = {
+    temperature: 20,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+};
+
+const data2 = {
+    temperature: 15,
+    weatherState: CLOUDY,
+    humidity: 10,
+    wind: '30 m/s',
+};
+
+
+class WeatherLocation extends Component{
+    
+    constructor() {
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data: data1
+        }
+    };
+
+    handleUpdateClick = () => {
+            this.setState  ({
+                data: data2
+            });
+            console.log("actualizado");
+             }
+
+        render = () => {
+            const {city, data} = this.state;
+            return(
+            <div className='weatherLocationCont'>
+                <Location city={city}/>
+                <WeatherData data={data} />
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>)
+        }
+
+    
+}
 
 
 export default WeatherLocation;
